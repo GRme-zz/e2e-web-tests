f96c674a95c26498e7480fd03995d3d7c0722da3
Martin Grambauer <martin.grambauer@googlemail.com>
2017-08-21 20:10:09 +0200

    updated stepdef

--- features/step_definitions/myStepDefinition.js
+++ features/step_definitions/myStepDefinition.js
@@ -1,26 +1,18 @@
-const {
-  client
-} = require('nightwatch-cucumber');
-const {
-  defineSupportCode
-} = require('cucumber');
+const { client } = require("nightwatch-cucumber");
+const { defineSupportCode } = require("cucumber");
 
 const myPage = client.page.myPageView();
 
-defineSupportCode(({Given, When, Then}) => {
-
-  When(/^go to google main page$/, async () => {
-    await myPage.goToGoogleMainPage(client);
-    return client;
+defineSupportCode(({ Given, When, Then }) => {
+  When(/^go to google main page$/, () => {
+    return myPage.goToGoogleMainPage(client);
   });
 
-  Then(/^all necessary elements are visible without async$/, async () => {
-    await myPage.checkAllElementsAreVisibleWithoutAsync(client);
-    return client;
+  Then(/^all necessary elements are visible without async$/, () => {
+    return myPage.checkAllElementsAreVisibleWithoutAsync(client);
   });
 
-  Then(/^all necessary elements are visible with async$/, async () => {
-    await myPage.checkAllElementsAreVisibleWithAsync(client);
-    return client;
+  Then(/^all necessary elements are visible with async$/, () => {
+    return myPage.checkAllElementsAreVisibleWithAsync(client);
   });
 });
